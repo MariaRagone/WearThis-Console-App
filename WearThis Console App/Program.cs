9a5ad2d082ef5de38f5c6d37f87493b09eca8909
Maria Ragone <maria@ragone.net>
2023-12-27 09:08:41 -0500

    updates to classes

--- WearThis Console App/Program.cs
+++ WearThis Console App/Program.cs
@@ -1,22 +1,3 @@
 ﻿//Creating file
 using WearThis_Console_App;
 
-string filePath = "../../../LibraryDatabase.txt";
-if (File.Exists(filePath) == false)
-{
-    StreamWriter tempWriter = new StreamWriter(filePath);
-    tempWriter.WriteLine("Red|false|Casual");
-    tempWriter.Close();
-}
-StreamReader reader = new StreamReader(filePath);
-List<Top> searchResults = new List<Top>();
-List<Top> tops = new List<Top>();
-List<Outfit> wearThis = new List<Outfit>();
-List<Outfit> wearing = new List<Outfit>();
-static void GetOutfit(Top top, Bottom bottom)
-
-{
-    Console.WriteLine($"Your outfit is {top.color} {top.type} with {bottom.color} {bottom.type}");
-    Outfit TopPart = new Top();
-    Outfit bottomPart = new Bottom();
-}
---
+++ WearThis Console App/Tops.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WearThis_Console_App
+{
+    internal class Top : Outfit //this is a child class of the type outfit
+    {
+        //properties
+        //all of the partent properties come down to the child (color, hasPattern, category)
+        public string sleeveLength { get; set; } //strapless, tank, cap, short, 3/4, long
+        public string fit { get; set; } //fitted, loose
+        public string length { get; set; } //short,medium, long
+        public string type { get; set; } //blouse, cardigan, collared, sweater, tank, t-shirt
+
+        //constructors
+        public Top(string _color, bool _hasPattern, string _category, string _sleeveLength,  string _fit, string _length, string _type)
+            : base //relates back to parent constructor, defines what part of the constructor is the parent
+            (_color, _hasPattern, _category)
+        {
+            sleeveLength = _sleeveLength;
+            fit = _fit;
+            length = _length;
+            type = _type;
+
+        }
+
+        //methods
+    }
+}
