129e2d23e7dbb1d73595f6c649c6a4d77048a3f6
Maria Ragone <maria@ragone.net>
2024-01-04 15:24:13 -0500

    adding run program logic for displaying and adding clothing

--- WearThis Console App/Program.cs
+++ WearThis Console App/Program.cs
@@ -2,6 +2,7 @@
 using System.Numerics;
 using System.Reflection;
 using System.Drawing;
+using System.ComponentModel.DataAnnotations;
 
 List<Top> TopsList = new List<Top>()
 {
@@ -38,41 +39,62 @@ List<Bottom> BottomsList = new List<Bottom>()
     new Bottom("Brown", true, "Dressy", "Short", "Loose", "Shorts"),
     new Bottom("Salmon", false, "Casual", "Skinny", "Ankle", "Jeans"),
 };
-
-//RUN THE PROGRAM
-Console.WriteLine("Welcome to Wear This! I will help you decide what to wear today using the clothin you already own.");
-Console.WriteLine("Make a selection.");
-Console.WriteLine("What type of clothing would you like to add?");
-
-Console.WriteLine("1. View Closet, 2. Add Clothing");
-int clothingChoice = 0;
-while(int.TryParse(Console.ReadLine(), out clothingChoice) ==false || clothingChoice <1 || clothingChoice >2)
+//welcome user
+Console.WriteLine("Welcome to Wear This! I will help you decide what to wear today using the clothing you already own.");
+//loop the program
+bool runProgram = true;
+while (runProgram)
 {
-    { Console.WriteLine("Invalid response. Try again"); } //error message
-    if (clothingChoice == 1) //display the closet
+    int menuChoice = -1;
+    //displaying the menue (view closet or add clothing)
+    while (menuChoice <= -1 || menuChoice >= 3)
+    {
+        DisplayMenu();
+        while (int.TryParse(Console.ReadLine(), out menuChoice) == false)
+        {
+            {
+                Console.WriteLine("Invalid response. Try again");//error message 
+            }
+        }
+    }
+    if (menuChoice == 1) //display the closet
     {
         Console.WriteLine("Tops:");
         DisplayTops(TopsList);
         Console.WriteLine("Bottoms:");
         DisplayBottoms(BottomsList);
     }
+    else if (menuChoice == 2) //add clothing
+    {
+        Console.WriteLine("What type of clothing would you like to add? 1. Top, 2. Bottom");
+    }
+    else if (menuChoice == 3)
+    {
+        Console.WriteLine("Bye!");
+        runProgram = false;
+        break;
+    }
 }
-DisplayTops(TopsList);
 
-bool runProgram = true;
-while (runProgram)
-{
-    DisplayTops(TopsList);
+//runProgram = Validator.GetContinue("Do you want to view another category?");
 
-}
+//DisplayTops(TopsList);
 
-static void DisplayTops(List<Top>fakeTopList)
+//while (runProgram)
+//{
+//    DisplayTops(TopsList);
+
+//}
+
+
+//METHODS
+static void DisplayTops(List<Top> fakeTopList)
 {
     Console.WriteLine($"{"Color",20} {"Has Pattern",20} {"Category",20} {"Sleeve Length",20} {"Fit",20} {"Length",20} {"Type",20}");
 
-    for (int i = 0; i<fakeTopList.Count; i++)
+    for (int i = 0; i < fakeTopList.Count; i++)
     {
-        Console.WriteLine($"{i+1}. {fakeTopList[i]}");
+        Console.WriteLine($"{i + 1}. {fakeTopList[i]}");
     }
     Console.WriteLine($"{fakeTopList.Count + 1}. Add Top");
     Console.WriteLine($"{fakeTopList.Count + 2}. Quit");
@@ -86,7 +108,48 @@ static void DisplayBottoms(List<Bottom> fakeBottomList)
     {
         Console.WriteLine($"{i + 1}. {fakeBottomList[i]}");
     }
-    Console.WriteLine($"{fakeBottomList.Count + 1}. Add Top");
+    Console.WriteLine($"{fakeBottomList.Count + 1}. Add Bottom");
     Console.WriteLine($"{fakeBottomList.Count + 2}. Quit");
 
+}
+static void DisplayMenu()
+{
+    Console.WriteLine("Choose an option:");
+    Console.WriteLine($"1. View Closet");
+    Console.WriteLine($"2. Add Clothing");
+    Console.WriteLine($"3. Quit");
+}
+
+static void addClothing()
+{
+    Console.WriteLine("What type of clothing would you like to add? 1. Top, 2. Bottom");
+    int clothingChoice = -1;
+    //displaying the menue (view closet or add clothing)
+    while (clothingChoice <= -1 || clothingChoice >= 2)
+    {
+        while (int.TryParse(Console.ReadLine(), out clothingChoice) == false)
+        {
+            {
+                Console.WriteLine("Invalid response. Try again");//error message 
+            }
+        }
+    }
+    if (clothingChoice == 1)
+    {
+        Console.WriteLine("What color is it?");
+        //pretent these are all validated
+        string color = Console.ReadLine();
+        Console.WriteLine("Does it have a pattern?");
+        string pattern = Console.ReadLine();
+        Console.WriteLine("Is it casual, dressy, business casual, or business?");
+        string category = Console.ReadLine();
+        Console.WriteLine("What is the sleeve length: strapless, tank, cap, short, 3/4, long");
+        string sleeveLength = Console.ReadLine();
+        Console.WriteLine("Is it fitted or loose?");
+        string fit = Console.ReadLine();
+        Console.WriteLine("Is it short,medium, or long");
+        string length = Console.ReadLine();
+        Console.WriteLine("Is it a blouse, cardigan, collared, sweater, tank, or t-shirt");
+        string type = Console.ReadLine();
+    }
 }
---
+++ WearThis Console App/Validator.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_10_CIRCLE_LAB
+{
+    public class Validator //public means you can use it in your other projects without any problems
+    {
+        public static int GetPositiveInputInt()
+        {
+            int result = -1;
+            while (int.TryParse(Console.ReadLine(), out result) == false || result <= 0)
+            {
+                Console.WriteLine("Invalid input. Try again with a positive number.");
+
+            }
+            return result;
+        }
+
+
+        //public static string GetUserInputString()
+        //{
+        //    string result = "";
+        //    while (result == )
+        //    { }
+        //    return result;
+        //}
+
+        public static bool GetContinue()
+        {
+            bool result = false;
+            while (true)
+            {
+                Console.WriteLine("Would you like to start again? y/n");
+                string choice = Console.ReadLine().Trim().ToLower();
+                if (choice == "y")
+                {
+                    result = true;
+                    break;
+                }
+                else if (choice == "n")
+                {
+                    result = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Try again.");
+                }
+            }
+            return result;
+        }
+
+        public static bool GetContinue(string message)
+        {
+            bool result = false;
+            while (true)
+            {
+                Console.WriteLine($"{message} y/n");
+                string choice = Console.ReadLine().Trim().ToLower();
+                if (choice == "y")
+                {
+                    result = true;
+                    break;
+                }
+                else if (choice == "n")
+                {
+                    result = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Try again.");
+                }
+            }
+            return result;
+        }
+
+        public static bool numberCheck(int x, int y) //static means you don't need to create an instance of it in order to use the method
+        {
+            if (x > y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+         
+       
+    }
+}
+
+
